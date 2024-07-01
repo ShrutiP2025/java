9481efaf10edbde069190de76bc2b24c5f78d3fb
ShrutiP2025 <shrutipmca@gmail.com>
2024-07-01 17:14:18 +0530

    Add files via upload

--- LowerBoundedWildCardDemo.java
+++ LowerBoundedWildCardDemo.java
@@ -1,25 +1,33 @@
-//Driver Program for lower bound wild card
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class LowerBoundedWildCardDemo {
 
-	public static void main(String args[]) {
-		List<Integer> intList = Arrays.asList(1, 2, 3);
-		System.out.println("Display Integer values");
-		//Display List of Integers
-		LowerBoundedWildCard.displayNumbers(intList);
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+        intList.add(4); // This works fine now
+        
+        System.out.println("Display Integer values");
+        LowerBoundedWildCardDemo.displayNumbers(intList);
 
-		List<Number> numberList = Arrays.asList(1.0, 2.0, 3.0,10,4,30);
-		System.out.println("Display Number values");
-		//Display List of Numbers
-		LowerBoundedWildCard.displayNumbers(numberList);
+        List<Number> numberList = new ArrayList<Number>(Arrays.asList(1.0, 2.0, 3.0, 10, 4, 30));
+        System.out.println("Display Number values");
+        LowerBoundedWildCardDemo.displayNumbers(numberList);
 
-		/*
-		 * List<Double> doubleList= Arrays.asList(1.0,2.0,3.0);
-		 * System.out.println("Display Double values");
-		 *  displayNumbers(doubleList);
-		 */
-	}
+        // Uncommenting this section will cause an error
+        /*
+         * List<Double> doubleList = Arrays.asList(1.0, 2.0, 3.0);
+         * System.out.println("Display Double values");
+         * LowerBoundedWildCardDemo.displayNumbers(doubleList);
+         */
+    }
+
+    // Sample method to display elements of a List<? extends Number>
+    public static void displayNumbers(List<? extends Number> list) {
+        for (Number num : list) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
