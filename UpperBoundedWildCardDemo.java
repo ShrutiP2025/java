98c9a5244fc55f77f43a0be0af6cff0c0fe91a05
ShrutiP2025 <shrutipmca@gmail.com>
2024-07-01 17:22:07 +0530

    Add files via upload

--- UpperBoundedWildCardDemo.java
+++ UpperBoundedWildCardDemo.java
@@ -1,28 +1,30 @@
-//Driver Program for upper bound wild card
-package com.tnsif.dayfifteen.generics;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class UpperBoundedWildCardDemo {
 
-	public static void main(String[] args) {
-		ArrayList<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(20);
-		//passing Integer List 
-		System.out.println("sum = " + UpperBoundedWildCard.sumOfList(intList));
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(10);
+        intList.add(20);
+        //passing Integer List 
+        System.out.println("sum = " + UpperBoundedWildCard.sumOfList(intList));
 
-		ArrayList<Double> doubleList = new ArrayList<Double>();
-		doubleList.add(10.0);
-		doubleList.add(20.0);
-		doubleList.add(30.0);
-		//passing Double List
-		System.out.println("sum = " + UpperBoundedWildCard.sumOfList(doubleList));
-		
-		/*
-		 * ArrayList<String> strList=new ArrayList<String>(); strList.add("Arun");
-		 * strList.add("Ajay"); strList.add("Ragul"); //passing String List
-		 * System.out.println("sum = " + UpperBoundedWildCard.sumOfList(strList));
-		 */
-		}
+        ArrayList<Double> doubleList = new ArrayList<Double>();
+        doubleList.add(10.0);
+        doubleList.add(20.0);
+        doubleList.add(30.0);
+        //passing Double List
+        System.out.println("sum = " + UpperBoundedWildCard.sumOfList(doubleList));
+
+        // Uncommenting this section will cause an error
+        /*
+         * ArrayList<String> strList=new ArrayList<String>();
+         * strList.add("Arun");
+         * strList.add("Ajay");
+         * strList.add("Ragul");
+         * //passing String List
+         * System.out.println("sum = " + UpperBoundedWildCard.sumOfList(strList));
+         */
+    }
 }
+
